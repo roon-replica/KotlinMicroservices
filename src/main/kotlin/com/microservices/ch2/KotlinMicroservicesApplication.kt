2317e0e05b9507f0b979e4b83ed8c1e5ce8c37b3
leemr <think-roon@naver.com>
2022-06-23 20:22:29 +0900

    1번째 코틀린 컨트롤러 작성!!

--- src/main/kotlin/com/microservices/ch2/KotlinMicroservicesApplication.kt
+++ src/main/kotlin/com/microservices/ch2/KotlinMicroservicesApplication.kt
@@ -2,9 +2,20 @@ package com.microservices.ch2
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
 
 @SpringBootApplication
 class KotlinMicroservicesApplication
+
+@Controller
+class FirstController {
+    @RequestMapping(value = ["hello"], method = [RequestMethod.GET])
+    @ResponseBody
+    fun hello() = "this is my first kotlin controller!!!!"
+}
 
 fun main(args: Array<String>) {
     runApplication<KotlinMicroservicesApplication>(*args)
