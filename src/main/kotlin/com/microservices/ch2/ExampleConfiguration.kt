92c681dd412f4c88048cf3ade19d37ab79f52403
leemr <think-roon@naver.com>
2022-06-23 22:53:48 +0900

    명시적 bean 생성

---
+++ src/main/kotlin/com/microservices/ch2/ExampleConfiguration.kt
@@ -0,0 +1,15 @@
+package com.microservices.ch2
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ExampleConfiguration {
+// 명시적 Bean 생성
+
+//    @Bean
+//    fun exampleService(): ExampleService = ExampleServiceImpl()
+//
+//    @Bean
+//    fun exampleService2(): ExampleService = ExampleServiceImpl2()
+}
