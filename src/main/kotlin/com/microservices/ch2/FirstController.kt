96beac134ac196e39fa4f769351b77eabe502639
leemr <think-roon@naver.com>
2022-06-24 10:04:36 +0900

    부트스트랩 클래스 패키지 이동

--- src/main/kotlin/com/microservices/ch2/FirstController.kt
+++ src/main/kotlin/com/microservices/ch2/FirstController.kt
@@ -2,18 +2,8 @@ package com.microservices.ch2
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.ResponseBody
-
-@SpringBootApplication
-class KotlinMicroservicesApplication
+import org.springframework.web.bind.annotation.*
 
 @Controller
 class FirstController {
@@ -28,8 +18,4 @@ class FirstController {
     @GetMapping("/test/annotation/value")
     @ResponseBody
     fun testAnnotationValue() = exampleService.valueTest()
-}
-
-fun main(args: Array<String>) {
-    runApplication<KotlinMicroservicesApplication>(*args)
 }
