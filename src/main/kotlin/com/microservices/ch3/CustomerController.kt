38828b83b158059b63d5dcafe16c5cf8283c708e
leemr <think-roon@naver.com>
2022-06-24 12:06:49 +0900

    create customer , http 415 오류 해결

--- src/main/kotlin/com/microservices/ch3/CustomerController.kt
+++ src/main/kotlin/com/microservices/ch3/CustomerController.kt
@@ -1,9 +1,12 @@
 package com.microservices.ch3
 
+import jdk.jfr.ContentType
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.ConcurrentHashMap
@@ -36,5 +39,10 @@ class CustomerController {
 
     object SampleCustomer {
         val sample = Customer(-1, "anonymous object declaration test")
+    }
+
+    @PostMapping("/customer")
+    fun createCustomer(@RequestBody customer: Customer) {
+        customers[customer.id] = customer
     }
 }
